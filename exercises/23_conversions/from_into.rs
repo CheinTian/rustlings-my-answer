87f588756abf432c006d406619cddc05459c1f91
cltian <cltian@gitlab.cn>
2024-02-28 23:05:32 +0800

    my answer

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+
+        let v: Vec<&str> = s.split(",").collect();
+
+        if v.len() < 2 {
+            return Person::default();
+        }
+
+        return if v[0].len() != 0 && v[1].len() != 0 && v[1].parse::<usize>().is_ok() {
+            Person { name: String::from(v[0]), age: v[1].parse::<usize>().unwrap() }
+        } else {
+            Person::default()
+        }
+
     }
 }
 
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,34 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let v: Vec<&str> = s.split(",").collect();
+
+        if v.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        if v[0].len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        if v[1].len() == 0 {
+            return Err(ParsePersonError::ParseInt(v[1].parse::<usize>().unwrap_err()));
+        }
+
+        if v[1].parse::<usize>().is_err() {
+            return Err(ParsePersonError::ParseInt(v[1].parse::<usize>().unwrap_err()));
+        }
+
+        return if v[0].len() != 0 && v[1].len() != 0 && v[1].parse::<usize>().is_ok() {
+            Ok(Person { name: String::from(v[0]), age: v[1].parse::<usize>().unwrap() })
+        } else {
+            Err(ParsePersonError::NoName)
+        }
+
     }
 }
 
